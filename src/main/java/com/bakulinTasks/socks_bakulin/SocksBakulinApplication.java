635683f1d166c37d4183b596abafbcbddcbea41c
EvgenyBakulin <ugend84@yandex.ru>
2024-12-23 22:36:52 +0300

    Commit 1

--- src/main/java/com/bakulinTasks/socks_bakulin/SocksBakulinApplication.java
+++ src/main/java/com/bakulinTasks/socks_bakulin/SocksBakulinApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SocksBakulinApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SocksBakulinApplication.class, args);
 	}
 
