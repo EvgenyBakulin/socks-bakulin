7c055c86a07231b766a71ce11e8981c938101d7f
EvgenyBakulin <ugend84@yandex.ru>
2024-12-23 16:49:49 +0300

    Initial commit

---
+++ src/main/java/com/bakulinTasks/socks_bakulin/SocksBakulinApplication.java
@@ -0,0 +1,13 @@
+package com.bakulinTasks.socks_bakulin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksBakulinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksBakulinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakulinTasks/socks_bakulin/SocksBakulinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakulinTasks.socks_bakulin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksBakulinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
